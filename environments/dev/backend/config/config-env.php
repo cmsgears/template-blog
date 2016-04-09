012296e334171061afed9dff3f24579903ab1772
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2016-04-09 14:48:51 +0530

    Updated environments.

--- environments/dev/backend/config/config-env.php
+++ environments/dev/backend/config/config-env.php
@@ -12,10 +12,10 @@ $config = [
 if( !YII_ENV_TEST ) {
 
     $config['bootstrap'][] 		= 'debug';
-    $config['modules']['debug'] = 'yii\debug\Module';
+    $config['modules']['debug'] = [ 'class' => 'yii\debug\Module' ];
 
     $config['bootstrap'][] 		= 'gii';
-    $config['modules']['gii'] 	= 'yii\gii\Module';
+    $config['modules']['gii'] 	= [ 'class' => 'yii\gii\Module' ];
 }
 
 return $config;
